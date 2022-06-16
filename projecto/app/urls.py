a054b08ff313bfc6088661bfafaaf1289d00a3ff
jimmysi2so <101122177+jimmysi2so@users.noreply.github.com>
2022-06-16 14:36:56 -0400

    Delete projecto directory

--- projecto/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, contacto, verproducto, productos
-
-urlpatterns = [
-    path('', home, name="home"),
-    path('contacto/', contacto, name="contacto"),
-    path('verproducto/', verproducto, name="verproducto"),
-    path('productos/', productos, name="productos"),
-]
